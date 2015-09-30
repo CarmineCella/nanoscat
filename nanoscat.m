2c0be059813d02a2e7dbc2acceb61c819a145f93
CarmineCella <Carmine@Aristotele.local>
2015-09-30 11:30:22 +0200

    Several corrections and improvements for readibility

--- nanoscat.m
+++ nanoscat.m
@@ -3,14 +3,19 @@
 % pedagogical implementation of scattering transform
 % (dyadic wavelets)
 
+clear all
+close all
+
 %% parameters
 
-M = 2;					% Scattering order
-J = 19;					% Maximal scale corresponding to T=Q*2^(J/Q+1)
+M = 3;					% Scattering order
+J = 11;					% Maximal scale corresponding to T=Q*2^(J/Q+1)
+
+fprintf ('computing %d orders, with %d bands (equiv to smoothing each %d samples)\n', M, J, 2^J);
 
 %% load audio
 
-[y, sr] = audioread ('../../datasets/various_data/3_onsets.wav');
+[y, sr] = audioread ('../../datasets/various_data/5_notes.wav');
 y = y(1:2^floor(log2(length(y)))); % truncate to power of 2
 y = y / norm(y);
 
@@ -71,22 +76,25 @@ end
 
 %% plot filters
 close all
-for m = 1:numel(psif)
-    figure
-    for i = m:numel(psif{m})
-        fprintf ('psi %d %d, length = %d\n', m, i, length (psif{m}{i}{1}));
-        v = psif{m}{i}{1};
-        plot (v)
-        hold on
-    end
+%for m = 1:numel(psif)
+m = 1;
+figure
+for i = m:numel(psif{m})
+    fprintf ('psi %d %d, length = %d\n', m, i, length (psif{m}{i}{1}));
+    v = psif{m}{i}{1};
+    plot (v)
+    hold on
 end
-%%
+%end
+title ('Band pass filters (PSI)');
 
+figure
 for i = 1:numel(phif)
-    figure
     plot (phif{i})
+    hold on
     fprintf ('phi %d, length = %d\n', i, length(phif{i}))
 end
+title ('Low pass filters (PHI)')
 
 %% convolve and subsample
 
@@ -98,7 +106,6 @@ next_bands=@(j) (max(0,j+delta));
 downsampling_fac =@(res,j) max(0,floor(j * log2(a)-res-aa));
 downsampling_fac_psi = @(res,j) max(0,floor(j * log2(a)-res-aa_psi));
 
-%%
 SC{1}{1}.signal = y;
 SC{1}{1}.meta.scale=-1;
 SC{1}{1}.meta.orientation=0;
@@ -109,13 +116,13 @@ for m=1:M+1
     if m > size(SC,2)		% No more coefficients to calculate
         continue;
     end
-
+    
     for s=1:numel(SC{m})
         sig=SC{m}{s}.signal;
         infos=SC{m}{s}.meta;
         
         % Decompose/propagate - retrieve high frequencies
-        if m<=M            
+        if m<=M
             number_of_j = length(psif{1});
             sigf=fft(sig);
             res=infos.resolution;
@@ -124,76 +131,73 @@ for m=1:M+1
             children={};
             for j=max(0,next_bands(prev_j)):numel(psif{res+1})-1
                 number_of_orientation = numel(psif{res+1}{j+1});
-                for th=1:number_of_orientation
-                    ds = downsampling_fac_psi(infos.resolution,j);
-                    ds2 = 2^ds;
-                    out = abs (ifft (sigf .* psif{res+1}{j+1}{th}));
-                   if ds2 > 1
-                       out = out(1:ds2:end)*sqrt(ds2);
-                   end
-                    
-                    children{raster}.signal = out;
-                    children{raster}.meta.resolution = res+ds;
-                    children{raster}.meta.scale = (infos.scale>=0)*infos.scale*number_of_j + j;
-                    children{raster}.meta.orientation = infos.orientation*number_of_orientation + th-1;
-                    
-                    raster=raster+1;
+                th = 1; % single orientation
+                %for th=1:number_of_orientation
+                ds = downsampling_fac_psi(infos.resolution,j);
+                ds2 = 2^ds;
+                out = abs (ifft (sigf .* psif{res+1}{j+1}{th}));
+                if ds2 > 1
+                    out = out(1:ds2:end)*sqrt(ds2);
                 end
+                
+                children{raster}.signal = out;
+                children{raster}.meta.resolution = res+ds;
+                children{raster}.meta.scale = (infos.scale>=0)*infos.scale*number_of_j + j;
+                children{raster}.meta.orientation = infos.orientation*number_of_orientation + th-1;
+                
+                raster=raster+1;
+                %end
             end
             for ic=1:numel(children)
                 SC{m+1}{raster1}=children{ic};
                 raster1=raster1+1;
             end
         end
-
+        
         sigf=fft(sig);
         number_of_j = length(psif{1});
         smoothed_sig.meta=infos;
         ds = downsampling_fac(infos.resolution,number_of_j)^2;
         smoothed_sig.signal = ifft(sigf .* phif{infos.resolution+1});
-       if ds > 1
-           smoothed_sig.signal = smoothed_sig.signal(1:ds:end)*sqrt(ds);
-       end
-     
+        if ds > 1
+            smoothed_sig.signal = smoothed_sig.signal(1:ds:end)*sqrt(ds);
+        end
+        
         smoothed_sig.meta.orignorm = norm(sig(:));
         SJ{m}{s} = smoothed_sig;
+        
+    end
+end
+
+%% plot results: interpolation to have a fixed matrix -------------
+
+%U1 = zeros(numel(psif{1}), length (y));
+% yc = fft (y);
+% S1 = zeros(numel(psif{1}), length (y));
+%
+% for i = 1:numel(psif{1})
+%     U1(i, :) = abs(ifft(yc .* psif{1}{i}{1}));
+% end
+
+%figure
+%imagesc (U1);
+
+for m =1:M+1
+    Out = SJ{m};
     
-    end
-end
-
-%% format output -------------
-
-yc = fft (y);
-U1 = zeros(numel(psif{1}), length (y));
-S1 = zeros(numel(psif{1}), length (y));
-
-for i = 1:numel(psif{1})
-    U1(i, :) = abs(ifft(yc .* psif{1}{i}{1}));
-end
-%%
-figure
-imagesc (U1);
-
-O1 = SC{2};
-maxlen = length (O1{1}.signal);
-nlambdas = numel(O1);
-
-Scoeff = zeros(nlambdas, maxlen);
-
-for i = 1:nlambdas
-    itp = interpft(O1{i}.signal, maxlen);
-    %sig = O1{i}.signal;
-    %Scoeff(i, 1:length(sig)) = sig;
-    Scoeff(i, :) = itp;
-end
-
-figure 
-imagesc (Scoeff)
-
-%%
-
-for m = 1:M+1
-    for i = 1:numel(SJ{m})
+    maxlen = length (Out{1}.signal);
+    nlambdas = numel(Out);
+    
+    Scoeff = zeros(nlambdas, maxlen);
+    
+    for i = 1:nlambdas
         fprintf ('order %d, lambda %d, coeff num = %d\n', m, i, length (SC{m}{i}.signal));
+        itp = interpft(Out{i}.signal, maxlen);
+        %Scoeff(i, 1:length(sig)) = Out{i}.signal;
+        Scoeff(i, :) = itp;
     end
+    
+    figure
+    imagesc (Scoeff)
+    title (sprintf ('Order %d', m - 1))
 end
